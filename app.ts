9073937a72b1c94bf58d0b8be71d94a6f650d8c8
abdussalim <abdussalimsan@gmail.com>
2024-04-16 17:45:28 +0700

    feat(apps): added app.ts as main program

---
+++ app.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+
+// Import environment variables
+import { PORT, APP_NAME } from './src/helpers/helper.environment';
+
+// Import controllers
+import controllerFoodRecognizer from './src/controllers/controller.food-recognizer';
+import controllerFoodInformationDetail from './src/controllers/controller.food-information-detail';
+
+// Import utils
+import utilFoodInformationDetail from './src/utils/util.food-information-detail';
+
+const app = express();
+
+// Middleware to parse request bodies as JSON
+app.use(express.json());
+
+const { getFoodName } = controllerFoodRecognizer;
+const { getFoodInformationDetail } = controllerFoodInformationDetail;
+const { doAuth, callback } = utilFoodInformationDetail;
+
+app.get('/auth', doAuth);
+app.get('/callback', callback); // Added missing '/' before 'callback'
+app.post('/food-image-recognize', getFoodName);
+app.get('/food-information-detail', getFoodInformationDetail);
+
+app.listen(PORT, () => {
+    console.log('Welcome to', APP_NAME);
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
