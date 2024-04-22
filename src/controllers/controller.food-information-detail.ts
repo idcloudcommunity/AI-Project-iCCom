be882d22ec2b85b82ac90f27e055e9e478564d55
abdussalim <abdussalimsan@gmail.com>
2024-04-22 18:15:18 +0700

    feat(controllers): added food-information-detail.ts and food-recognizer.ts

--- src/controllers/controller.food-information-detail.ts
+++ src/controllers/controller.food-information-detail.ts
@@ -1,39 +1,28 @@
-import { Request, Response, } from 'express';
-import axios from 'axios';
+import { Request, Response } from 'express';
 
-//util export
-import utilFoodInformationDetail from '../utils/util.food-information-detail';
+//import services
+import foodInformationDetailService from '../services/service.food-information-detail';
+
+interface Nurtrition {
+    foodName: String
+}
 
 const controllerFoodInformationDetail = {
-    getFoodInformationDetail: async (request: Request, response: Response) => {
-        try {
-            // Assuming you have the access token stored in a variable
-            const accessToken = 'YOUR_ACCESS_TOKEN';
-    
-            // Make the request to the API
-            const result = await axios.post(
-                'https://platform.fatsecret.com/rest/server.api',
-                {
-                    method: 'food.get.v2',
-                    food_id: '33691',
-                    format: 'json'
-                },
-                {
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${accessToken}`
-                    }
-                }
-            );
-    
-            // Send the response from the API to the client
-            response.status(200).json(result.data);
-        } catch (error) {
-            // Handle errors
-            console.error('Error:', error);
-            response.status(500).json({ error: 'Internal server error' });
-        }
+  getFoodInformationDetail: async (request: Request, response: Response) => {
+    try {
+
+        const nutrition: Nurtrition = request.body;
+        const query: String = nutrition.foodName || 'tofu and tempeh';
+
+      const finalResult = await foodInformationDetailService(query);
+      // Send the response from the API to the client
+      response.status(200).json(finalResult);
+    } catch (error: any) {
+      // Handle errors
+      console.error('Error:', error);
+      response.status(500).json({ error: 'Internal server error' });
     }
-}
+  },
+};
 
 export default controllerFoodInformationDetail;
