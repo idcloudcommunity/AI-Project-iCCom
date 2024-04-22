b24d7bb4fc24430ef53dab4ed17c5c5436abd870
abdussalim <abdussalimsan@gmail.com>
2024-04-22 18:16:48 +0700

    feat(helpers): added environtment.ts and types.ts

--- src/helpers/helper.environment.ts
+++ src/helpers/helper.environment.ts
@@ -8,4 +8,10 @@ dotenv.config();
 // Export the environment variable
 export const PORT  = process.env.TYPES_EXPRESS_PORT as unknown as Environment;
 export const APP_NAME  = process.env.TYPES_EXPRESS_NAME as unknown as Environment;
+
+//claude AI
 export const CLAUDEAI_API_KEY  = process.env.TYPES_EXPRESS_CLAUDEAI_API_KEY?.toString() as unknown as Environment;
+export const CLAUDEAI_PROMPT  = process.env.TYPES_EXPRESS_CLAUDEAI_PROMPT?.toString() as unknown as Environment;
+
+//Api Ninjas - Nutrition
+export const NUTRITION_API_KEY  = process.env.TYPES_EXPRESS_API_NINJAS_NUTRITION_API_KEY?.toString() as unknown as Environment;
--- src/helpers/helper.types.ts
+++ src/helpers/helper.types.ts
@@ -1,5 +1,7 @@
 export interface Environment {
     APP_NAME: string
     PORT: number    
-    CLAUDEAI_API_KEY: any    
+    CLAUDEAI_API_KEY: any  
+    CLAUDEAI_PROMPT: any
+    NUTRITION_API_KEY: any
 }
