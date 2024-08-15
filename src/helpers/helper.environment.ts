d76039271f51c5e5e63e98b9b91c1b799eec90c5
abdussalim <abdussalimsan@gmail.com>
2024-08-15 14:40:24 +0700

    fix(environment): fix type of some variables to string

--- src/helpers/helper.environment.ts
+++ src/helpers/helper.environment.ts
@@ -6,8 +6,8 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 // Export the environment variable
-export const PORT  = process.env.TYPES_EXPRESS_PORT as unknown as Environment;
-export const APP_NAME  = process.env.TYPES_EXPRESS_NAME as unknown as Environment;
+export const PORT  = process.env.TYPES_EXPRESS_PORT?.toString() as unknown as Environment;
+export const APP_NAME  = process.env.TYPES_EXPRESS_NAME?.toString() as unknown as Environment;
 
 //claude AI
 export const CLAUDEAI_API_KEY  = process.env.TYPES_EXPRESS_CLAUDEAI_API_KEY?.toString() as unknown as Environment;
